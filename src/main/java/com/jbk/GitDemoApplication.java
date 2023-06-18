60b4f2cc4ac1a1cd1d113fe1d6844cce4b907e37
nileshDhaigude <nileshdhaigude@007gmail.com>
2023-06-17 18:35:08 -0800

    something new

--- src/main/java/com/jbk/GitDemoApplication.java
+++ src/main/java/com/jbk/GitDemoApplication.java
@@ -8,6 +8,8 @@ public class GitDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitDemoApplication.class, args);
+		
+		System.out.println("some thing new in code");
 	}
 
 }
